ad70631c2c94440f3479a741278a7c23e662dbab
James Bennison <james_bennison@192.168.1.104>
2023-06-09 19:00:39 +0100

    init commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
